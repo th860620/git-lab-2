c2d20efbc1f097c4077ee6446d47d90888725576
th860620 <th860620@ohio.edu>
2021-09-01 17:50:06 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Seungtak Han>
+ *        Date: <09/01/2021>
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
